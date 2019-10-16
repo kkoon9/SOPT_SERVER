1f2b2219314fbef45b4e78859c685248087b149e
kkoon9 <rndrnjs2003@naver.com>
2019-10-16 17:36:48 +0900

    업로드 SOPT 2차 세미나 과제 [2] lv1

---
+++ project2_lv1/routes/api/group.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var router = express.Router();
+var csv = require('csvtojson');
+var filePath = path.join("C:/Users/rndrn/Documents/sopt-server/project2_lv1/public/csvs/member.csv");
+/* API page. */
+router.get('/', function(req, res, next) {
+    csv().fromFile(filePath).then((jsonArr) => {
+        if(!jsonArr) {
+            console.log(`file read err: ${err}`);
+            res.send(err);
+        }
+        else{
+            res.send(jsonArr);
+            console.log("success printing member");   
+        }
+    });
+ 
+});
+router.get('/:gId', function(req, res, next) {
+    var names = [];
+    csv().fromFile(filePath).then((jsonArr) => {
+        var gId = req.params.gId;
+        console.log(`gId : ${gId}`);
+        if(!jsonArr) {
+            console.log(`file read err: ${err}`);
+            res.send(err);
+        }
+        else{
+            names.push(gId+"조");
+            for(var GIdx in jsonArr){
+                if(gId != jsonArr[GIdx].groupIdx) {
+                    console.log(jsonArr[GIdx].groupIdx);
+                    continue;
+                }
+                else{
+                    console.log(jsonArr[GIdx].name);
+                    names.push(jsonArr[GIdx].name);
+                }
+            }
+        }
+        res.send(names);
+        console.log("success printing member");       
+    });
+
+});
+module.exports = router;
